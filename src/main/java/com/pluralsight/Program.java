417fcf212595085ec0a6e7c7735cf0db9122d382
michalhagos <mhayele3@gmail.com>
2026-05-12 14:24:50 -0500

    Added main method to Program class that creates UserInterface and calls display

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
 public class Program {
+    public static void main(String[] args) {
+        // creating an instance/ object of the UserInterface class called ui
+        // this is the only thing main needs to do for me to run my application
+        UserInterface ui = new UserInterface();
+
+        // calling display to start the menu and run the application
+        ui.display();
+    }
+
+
+
+
+
+
 }
