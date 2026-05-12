3435b5ca23ffc9860ee94f33a82cc69af860f681
michalhagos <mhayele3@gmail.com>
2026-05-12 11:18:38 -0500

    Added constructor to Vehicle class with all fields as parameters

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -18,6 +18,21 @@ public class Vehicle {
     // the price of the vehicle
     private double price;
 
+    // created a constructor sets all fields when a new vehicle is created
+    public Vehicle(int vin, int year, String make, String model,
+                   String vehicleType, String color, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
+
+
+
 
 
 
