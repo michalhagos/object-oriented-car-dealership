b123a1484e5387187d219b0dec3afd4781b55b57
michalhagos <mhayele3@gmail.com>
2026-05-12 13:00:23 -0500

    Read first line of CSV and create Dealership object from dealership info

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -18,14 +18,32 @@ public class DealershipFileManager {
         try {
             // open the CSV file for reading line by line
             BufferedReader inventoryReader = new BufferedReader(new FileReader(FILE_PATH));
+            // read the first line which contains the dealership info
+// format is: name|address|phone
+            String dealershipInfo = inventoryReader.readLine();
+
+            // split the first line by pipe to get the individual parts
+            String[] dealershipParts = dealershipInfo.split("\\|");
+
+            // extract each part of the dealership info
+            String name = dealershipParts[0];
+            String address = dealershipParts[1];
+            String phone = dealershipParts[2];
+
+// create the dealership object using the info from the first line
+            theDealership = new Dealership(name, address, phone);
+
 
         } catch (Exception e) {
             System.out.println("Error reading dealership file: " + e.getMessage());
         }
 
         return theDealership;
-    }
 
+
+
+
+    }
 
 
 
