9dc71f6168be69e214a7988ee8604893a8a43a49
michalhagos <mhayele3@gmail.com>
2026-05-12 11:17:29 -0500

    Added all private fields to Vehicle class

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,2 +1,30 @@
 public class Vehicle {
+
+    // creating the fields I need inside the vehicle class
+    // unique identifier for the vehicle the VIN number
+    private int vin;
+    // the year the vehicle was manufactured
+    private int year;
+    // the make of the vehicle - Ford, Honda, Toyota etc
+    private String make;
+    // the model of the vehicle example Explorer, Civic
+    private String model;
+    // the type of vehicle - car, truck, SUV, van
+    private String vehicleType;
+    // the color of the vehicle
+    private String color;
+    // how many miles the vehicle has been driven
+    private int odometer;
+    // the price of the vehicle
+    private double price;
+
+
+
+
+
+
+
+
+
+
 }
