9e33522dc8eaf6e2273b4ea13aa2414b78495417
michalhagos <mhayele3@gmail.com>
2026-05-12 12:27:18 -0500

    declared a constant variable called file path pointing to the inventory.csv in resources folder

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -2,6 +2,11 @@ package com.pluralsight;
 
 public class DealershipFileManager {
 
+    // the path to the inventory CSV file that holds the dealership and vehicle data
+
+    private final String FILE_PATH = "src/main/resources/inventory.csv";
+
+
 
 
 
