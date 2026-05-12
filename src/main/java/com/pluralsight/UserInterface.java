839b8e10854c490de6397a204df8ba915fb3645a
michalhagos <mhayele3@gmail.com>
2026-05-12 13:52:39 -0500

    Added private Dealership field to UserInterface class

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -2,5 +2,16 @@ package com.pluralsight;
 
 public class UserInterface {
 
+    // the dealership object this User interface will interact with
+    // loaded from the inventory.csv file when the app starts
+    private Dealership dealership;
+
+
+
+
+
+
+
+
 
 }
