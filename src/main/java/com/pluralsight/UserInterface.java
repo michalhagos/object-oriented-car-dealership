7ffff766dd5ee9a3150f35ab7c1383621d3b4226
michalhagos <mhayele3@gmail.com>
2026-05-12 14:02:52 -0500

    Add private init method to load dealership from CSV file

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -7,7 +7,14 @@ public class UserInterface {
     private Dealership dealership;
 
 
-
+    // this method loads the dealership from the CSV file
+    // called at the start of display() before the menu runs
+    private void init() {
+        // create the file manager so we can read the CSV
+        DealershipFileManager fileManager = new DealershipFileManager();
+        // load the dealership and store it in this class
+        this.dealership = fileManager.getDealership();
+    }
 
 
 
